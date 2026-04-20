a755fbd1e924e47e962c3b9d178f49b8ec042c3b
Deepti-Jana <deeptij.3690@gmail.com>
2026-04-20 22:33:08 +0530

    UC3: Accept user slot input (1-9)

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,24 +1,36 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * TicTacToe
  * Name- Deepti Jana
- * UC2 performs a random toss to decide who plays first and assigns
- * symbols (X or O) to the human and computer accordingly.
+ * UC3 Accept User Slot Input (1–9)
  */
+
 public class TicTacToe {
 
     static boolean isHumanTurn;
     static char humanSymbol;
     static char computerSymbol;
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         tossAndAssignSymbols();
         displayTossResult();
+
+        int slot = acceptUserInput();   // UC3
+        System.out.println("User selected slot: " + slot);
     }
+
+    /**
+     * UC2 Toss Logic
+     */
     static void tossAndAssignSymbols() {
 
         Random random = new Random();
-        int toss = random.nextInt(2);   // 0 or 1
+        int toss = random.nextInt(2);
 
         if (toss == 0) {
             isHumanTurn = true;
@@ -30,17 +42,27 @@ public class TicTacToe {
             computerSymbol = 'X';
         }
     }
+
     static void displayTossResult() {
 
         System.out.println("===== Toss Result =====");
 
-        if (isHumanTurn) {
-            System.out.println("Human won the toss and plays first.");
-        } else {
-            System.out.println("Computer won the toss and plays first.");
-        }
+        if (isHumanTurn)
+            System.out.println("Human plays first");
+        else
+            System.out.println("Computer plays first");
 
         System.out.println("Human Symbol: " + humanSymbol);
         System.out.println("Computer Symbol: " + computerSymbol);
+    }
+
+
+    static int acceptUserInput() {
+
+        System.out.println("Enter slot number (1-9): ");
+
+        int slot = scanner.nextInt();
+
+        return slot;
     }
 }
